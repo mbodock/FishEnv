6c6c7b9f7d04f6c826289f233efa68283d92e587
Marcus Vinicius (Mbodock) <mbodock@gmail.com>
2016-03-02 22:59:55 -0300

    Adds mktmpenv function
    Now its possible to create a temporary virtualenv.

--- fishenv.fish
+++ fishenv.fish
@@ -19,6 +19,58 @@ function mkvirtualenv -d 'Create virtualenv on the current directory'
     end
 end
 
+function mktmpenv -d 'Creates a temporary virtualenv'
+    set dir (pwd)
+    mkdir -p "/tmp/fishtmpenvs"
+    cd  "/tmp/fishtmpenvs"
+
+    if test -n $argv[1]
+        set -g -x ENV_NAME "$argv[1]"
+    else
+        set -g -x ENV_NAME (cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
+    end
+    echo "Creating $ENV_NAME..."
+    virtualenv $ENV_NAME $argv[2..-1]
+    cd $ENV_NAME
+    . ./bin/activate.fish
+    function deactivate -d "Exits and deletes virtualenv"
+        _original_deactivate
+        echo "removing files /tmp/fishtmpenvs/$ENV_NAME"
+        rm -rf "/tmp/fishtmpenvs/$ENV_NAME"
+        set -e ENV_NAME
+    end
+    cd $dir
+end
+
+
+function _original_deactivate  -d "Exit virtualenv and return to normal shell environment"
+    # reset old environment variables
+    if test -n "$_OLD_VIRTUAL_PATH"
+        set -gx PATH $_OLD_VIRTUAL_PATH
+        set -e _OLD_VIRTUAL_PATH
+    end
+    if test -n "$_OLD_VIRTUAL_PYTHONHOME"
+        set -gx PYTHONHOME $_OLD_VIRTUAL_PYTHONHOME
+        set -e _OLD_VIRTUAL_PYTHONHOME
+    end
+
+    if test -n "$_OLD_FISH_PROMPT_OVERRIDE"
+        # set an empty local fish_function_path, so fish_prompt doesn't automatically reload
+        set -l fish_function_path
+        # erase the virtualenv's fish_prompt function, and restore the original
+        functions -e fish_prompt
+        functions -c _old_fish_prompt fish_prompt
+        functions -e _old_fish_prompt
+        set -e _OLD_FISH_PROMPT_OVERRIDE
+    end
+
+    set -e VIRTUAL_ENV
+    if test "$argv[1]" != "nondestructive"
+        # Self destruct!
+        functions -e deactivate
+    end
+end
+
 function rmvirtualenv -d 'Removes <name> virtualenv'
     set -l envs (cat $HOME/.fishenvs/envs | grep -i "^$argv[1] "|cut -d' ' -f1 )
     set -l matchs (echo $envs| wc -w)
